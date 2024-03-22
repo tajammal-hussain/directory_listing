24dfbfe08a1982447e13c54feeadb500c2cf12b0
Waqar_Ahmad <waqar.ahmad.brame@gmail.com>
2024-03-23 02:41:13 +0500

    breeze installed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified', 'normal'])
+    ->name('dashboard');
+
+Route::view('admin', 'admin')
+    ->middleware(['auth', 'verified', 'admin'])
+    ->name('admin');
+
+Route::view('superadmin', 'superadmin')
+    ->middleware(['auth', 'verified', 'superadmin'])
+    ->name('superadmin');
+
+Route::view('main', 'main')->middleware(['auth','verified','main'])->name('main');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+
+
 Route::get('/', function () {
     return view('pages.dashboard');
 })->name('dashboard');
@@ -208,7 +231,3 @@ Route::get('/add_social_media', function () {
 Route::get('/add_customer', function () {
     return view('pages.add_customer');
 })->name('add_customer');
-
-
-
-
